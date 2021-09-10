ea00974f42f84d7fc031451cc702cbaf6a26d76c
Jack Hogan <jackhogan11@gmail.com>
2021-09-10 18:39:00 -0400

    Got uploading working, new version 0.0.3

--- index.js
+++ index.js
@@ -5,67 +5,68 @@ module.exports = (Plugin, Library) => {
         onStart() {
             // Set global modules
             this.fileCheckMod = WebpackModules.getByProps("anyFileTooLarge");
-            this.fileUploadMod = WebpackModules.getByProps("instantBatchUpload");
+            this.fileUploadMod = WebpackModules.getByProps("instantBatchUpload", "upload");
+
+            /**
+             * UPLOAD MODULE
+             */
 
             // Make all file too large checks succeed
-            Patcher.instead("", this.fileCheckMod.prototype, "anyFileTooLarge", (thisObj, args, original) => {
+            Patcher.instead(this.fileCheckMod, "anyFileTooLarge", (_, __, ___) => {
                 return false;
             });
 
             // Patch upload call to either pass file unaltered if under limit or chunked if over
-            Patcher.instead("", this.fileUploadMod.prototype, "upload", (thisObj, args, original) => {
-                const [channelId, file] = args;
-                const fileName = file.name;
-                const numChunks = Math.ceil(file.size / thisObj.maxFileUploadSize());
+            Patcher.instead(this.fileUploadMod, "upload", (_, args, original) => {
+                const [channelId, file, n] = args;
+                // Create a small buffer under limit
+                const numChunks = Math.ceil(file.size / this.maxFileUploadSize());
                 // Don't do anything if no changes needed
-                if (file.size <= thisObj.maxFileUploadSize()) {
-                    original(args);
+                if (numChunks == 1) {
+                    original(...args);
                     return;
-                } else if (file.size + numChunks * 4 / thisObj.maxFileUploadSize() > 255) { // Check to make sure the number of files when chunked with header is not greater than 255 otherwise fail
+                } else if ((file.size + numChunks * 4) / this.maxFileUploadSize() > 255) { // Check to make sure the number of files when chunked with header is not greater than 255 otherwise fail
                     BdApi.showToast("Unable to upload file: File size exceeds max chunk count of 255.", {type:"error"});
                     return;
                 }
 
                 BdApi.showToast("Generating file chunks...", {type:"info"});
 
-                // Create temp directory
-                const fs = require("fs");
-                const path = require("path");
-                const os = require("os");
-                const crypto = require("crypto");
-                const id = crypto.randomBytes(16).toString("hex");
-                let stagingDir;
-                try {
-                    stagingDir = fs.mkdtempSync(path.join(os.tmpdir(), `staging-${id}`));
-                } catch (e) {
-                    BdApi.showToast("Unable to upload file: Unable to create chunking directory.", {type:"error"});
-                    Logger.stacktrace(e);
-                    return;
-                }
-
                 // Convert file to bytes
                 file.arrayBuffer().then(buffer => {
-                    const fileBytes = Uint8Array(buffer);
+                    const fileBytes = new Uint8Array(buffer);
 
                     // Write files with leading bit to determine order
-                    for (let chunk = 0; chunk < numChunks; chunk++) {
-                        const baseOffset = chunk * thisObj.maxFileUploadSize();
-                        const bytesToWrite = fileBytes.slice(baseOffset, baseOffset + thisObj.maxFileUploadSize() - 1);
-                        // Write header: "DF" (discord file) then protocol version then chunk number then total chunk count
-                        bytesToWrite.unshift(0xDF, 0x00, chunk, numChunks);
-                        // Write file to temp directory
-                        fs.writeFileSync(path.join(stagingDir, `${chunk}-${fileName}.dlfc`));
-                    }
-
                     // Upload new chunked files
-                    for (let chunkFile = 0; chunkFile < numChunks; chunkFile) {
-                        args.file = new File([], path.join(stagingDir, `${chunkFile}-${fileName}.dlfc`));
-                        original(args);
+                    const fileList = [];
+                    for (let chunk = 0; chunk < numChunks; chunk++) {
+                        const baseOffset = chunk * this.maxFileUploadSize();
+                        // Write header: "DF" (discord file) then protocol version then chunk number then total chunk count
+                        const headerBytes = new Uint8Array(4);
+                        headerBytes.set([0xDF, 0x00, chunk & 0xFF, numChunks & 0xFF]);
+                        // Slice original file with room for header
+                        const bytesToWrite = fileBytes.slice(baseOffset, baseOffset + this.maxFileUploadSize() - 4);
+                        // Create new array
+                        const mergedArray = new Uint8Array(headerBytes.length + bytesToWrite.length);
+                        mergedArray.set(headerBytes);
+                        mergedArray.set(bytesToWrite, headerBytes.length);
+                        // Add file to array
+                        fileList.push(new File([mergedArray], `${chunk}-${file.name}.dlfc`));
                     }
+                    this.fileUploadMod.instantBatchUpload(channelId, fileList, n);
                     
-                    BdApi.showToast("All files uploaded.", {type:"success"});
+                    BdApi.showToast("All files uploading...", {type:"success"});
                 });
             });
+
+            /**
+             * RENDER MODULE
+             */
+
+
+            /**
+             * DOWNLOAD MODULE
+             */
 
             Logger.log("Initialization complete");
         }
@@ -74,7 +75,8 @@ module.exports = (Plugin, Library) => {
         maxFileUploadSize() {
             if (!this.fileCheckMod) return 0;
 
-            return this.fileCheckMod.maxFileSize(DiscordAPI.currentGuild);
+            // Built-in buffer, otherwise file upload fails
+            return this.fileCheckMod.maxFileSize(DiscordAPI.currentGuild) - 1000;
         }
 
         onStop() {
